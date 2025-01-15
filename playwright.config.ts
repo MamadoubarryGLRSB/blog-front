d962b1bacf184a81a51fffe4da5937309f30ecfe
MamadoubarryGLRSB <mamadou.barry1@ism.edu.sn>
2025-01-15 22:47:03 +0100

    fix: run test

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,13 @@ export default defineConfig({
   testDir: './tests',
   use: {
     headless: true,
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3001', // Mis à jour pour utiliser le port 3001
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
+  },
+  webServer: {
+    command: 'npm run start',
+    port: 3001,
+    reuseExistingServer: !process.env.CI
   }
 });
